7514865b617e458f92a29f9d99ba6fe31ede3717
notaeris <georgemaster747@gmail.com>
2022-03-27 15:19:22 +0100

    Element handler changes

--- src/main/java/io/github/notaeris/moon/element/Element.java
+++ src/main/java/io/github/notaeris/moon/element/Element.java
@@ -1,20 +1,5 @@
 package io.github.notaeris.moon.element;
 
-import io.github.nosequel.command.CommandHandler;
-import io.github.notaeris.moon.MoonPlugin;
-import io.github.notaeris.moon.command.gamemode.adapter.GameModeTypeAdapter;
-import io.github.notaeris.moon.profile.Profile;
-import io.github.notaeris.moon.profile.adapter.ProfileTypeAdapter;
-import io.github.notaeris.moon.rank.Rank;
-import io.github.notaeris.moon.rank.adapter.RankTypeAdapter;
-import io.github.notaeris.moon.util.adapter.ChatColorTypeAdapter;
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.GameMode;
-import org.bukkit.event.Listener;
-
-import java.util.Arrays;
-
 public interface Element {
 
     /**
@@ -33,51 +18,5 @@ public interface Element {
      */
     default void unload(Element element) {
 
-    }
-
-    /**
-     * Find a {@link Element} by type class
-     *
-     * @param clazz the class
-     * @param <T> the type of class
-     * @return the type of element class
-     */
-    default <T> T findElement(Class<T> clazz) {
-        return null;
-    }
-
-    /**
-     * Initialise all listed {@link Element}s
-     *
-     * @param elements the elements
-     */
-    default void initialiseElements(Element... elements) {
-        Arrays.stream(elements)
-                .forEach(this::load);
-    }
-
-    /**
-     * Initialise all listed listeners
-     *
-     * @param listeners the listeners
-     */
-    default void initialiseListeners(Listener... listeners) {
-        Arrays.stream(listeners)
-                .forEach(listener -> Bukkit.getPluginManager().registerEvents(listener, MoonPlugin.getPlugin(MoonPlugin.class)));
-    }
-
-    /**
-     * Initialise all listed commands
-     *
-     * @param objects the commands
-     */
-    default void initialiseCommands(CommandHandler commandHandler, Object... objects) {
-        commandHandler.registerTypeAdapter(ChatColor.class, new ChatColorTypeAdapter());
-        commandHandler.registerTypeAdapter(Profile.class, new ProfileTypeAdapter());
-        commandHandler.registerTypeAdapter(Rank.class, new RankTypeAdapter());
-        commandHandler.registerTypeAdapter(GameMode.class, new GameModeTypeAdapter());
-
-        Arrays.stream(objects)
-                .forEach(commandHandler::registerCommand);
     }
 }
--- src/main/java/io/github/notaeris/moon/element/ElementHandler.java
+++ src/main/java/io/github/notaeris/moon/element/ElementHandler.java
@@ -1,22 +1,69 @@
 package io.github.notaeris.moon.element;
 
+import io.github.nosequel.command.CommandHandler;
+import io.github.notaeris.moon.MoonPlugin;
+import io.github.notaeris.moon.command.gamemode.adapter.GameModeTypeAdapter;
+import io.github.notaeris.moon.profile.Profile;
+import io.github.notaeris.moon.profile.adapter.ProfileTypeAdapter;
+import io.github.notaeris.moon.rank.Rank;
+import io.github.notaeris.moon.rank.adapter.RankTypeAdapter;
+import io.github.notaeris.moon.util.adapter.ChatColorTypeAdapter;
 import lombok.Getter;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
+import org.bukkit.event.Listener;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 @Getter
-public class ElementHandler implements Element {
+public class ElementHandler {
 
     private final Map<Class<? extends Element>, Element> elementMap = new HashMap<>();
 
-    @Override
     public void load(Element element) {
         this.elementMap.put(element.getClass(), element);
     }
 
-    @Override
     public <T> T findElement(Class<T> clazz) {
         return clazz.cast(elementMap.get(clazz));
+    }
+
+    /**
+     * Initialise all listed {@link Element}s
+     *
+     * @param elements the elements
+     */
+    public void initialiseElements(Element... elements) {
+        Arrays.stream(elements)
+                .forEach(this::load);
+    }
+
+    /**
+     * Initialise all listed listeners
+     *
+     * @param listeners the listeners
+     */
+    public void initialiseListeners(Listener... listeners) {
+        Arrays.stream(listeners)
+                .forEach(listener -> Bukkit.getPluginManager().registerEvents(listener, MoonPlugin.getPlugin(MoonPlugin.class)));
+    }
+
+
+    /**
+     * Initialise all listed commands
+     *
+     * @param objects the commands
+     */
+    public void initialiseCommands(CommandHandler commandHandler, Object... objects) {
+        commandHandler.registerTypeAdapter(ChatColor.class, new ChatColorTypeAdapter());
+        commandHandler.registerTypeAdapter(Profile.class, new ProfileTypeAdapter());
+        commandHandler.registerTypeAdapter(Rank.class, new RankTypeAdapter());
+        commandHandler.registerTypeAdapter(GameMode.class, new GameModeTypeAdapter());
+
+        Arrays.stream(objects)
+                .forEach(commandHandler::registerCommand);
     }
 }
